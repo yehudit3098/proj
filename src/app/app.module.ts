1bf074483278641b0b1e04e0b30a03c7e1285be4
Yehudit Ackerman <Yehudita@galileotech.local>
2021-10-13 10:56:07 +0300

    rebase 3 commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StamComponent } from './stam/stam.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StamComponent
   ],
   imports: [
     BrowserModule,
